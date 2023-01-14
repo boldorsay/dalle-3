0491c5c17869cd4249317657b6104f422d1328ba
boldorsay <72067985+boldorsay@users.noreply.github.com>
2023-01-14 13:22:08 +0100

    test AR

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import { exportCenter, transfert, transfertMiddle, showImage, saveTheImage } from "./action";
+import { addTexture } from "./sphere";
 import { createWebcam, cameraToImage } from "./webcam";
 
 const camera = document.querySelector("#videoCamera");
@@ -21,22 +22,21 @@ createWebcam({
   
 
   btnCamera.onclick = async () => {
-    btnCamera.disabled = true;
-    let imgScreen =  await cameraToImage(camera);
+    // btnCamera.disabled = true;
+    // let imgScreen =  await cameraToImage(camera);
     
-    const imageRight = await transfert(imgScreen, 0.3, 0.7, 0, "student bedroom", 1024)
-    const imgLeft = await transfert(imgScreen, 0, 0.3, 0.7, "student bedroom", 1024)
-    const [imageCenter, canvasLarge] = await exportCenter(imageRight, imgLeft)
+    // const imageRight = await transfert(imgScreen, 0.3, 0.7, 0, "student bedroom", 1024)
+    // const imgLeft = await transfert(imgScreen, 0, 0.3, 0.7, "student bedroom", 1024)
+    // const [imageCenter, canvasLarge] = await exportCenter(imageRight, imgLeft)
 
 
-    // display the image on the body
+    // // display the image on the body
     
-    const imageMiddle = await transfertMiddle(imageCenter, 1024, "student bedroom")
-    const imageFinal = await showImage(canvasLarge, imageMiddle)
-    document.body.appendChild(imageFinal);
+    // const imageMiddle = await transfertMiddle(imageCenter, 1024, "student bedroom")
+    // const imageFinal = await showImage(canvasLarge, imageMiddle)
+    // document.body.appendChild(imageFinal);
 
-
-
+    // addTexture(imageFinal)
     // const imageToThreejs = await saveTheImage(imageFinal)
 
   };
---
+++ sphere.js
@@ -0,0 +1,80 @@
+import * as THREE from "three";
+import { DeviceOrientationControls } from "./DeviceOrientationControls.js";
+
+let camera, scene, renderer, controls;
+let action = false;
+
+const startButton = document.getElementById("startButton");
+
+export function addTexture(texture) {
+  console.log(texture);
+}
+
+document.body.addEventListener(
+  "click",
+  function () {
+    init();
+    animate();
+  },
+  { once: true, useCapture: true }
+);
+
+function init() {
+  const overlay = document.getElementById("overlay");
+  overlay.remove();
+
+  camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    1,
+    1100
+  );
+
+  controls = new DeviceOrientationControls(camera);
+
+  scene = new THREE.Scene();
+
+  const geometry = new THREE.SphereBufferGeometry(500, 60, 40);
+  // invert the geometry on the x-axis so that all of the faces point inward
+  geometry.scale(-1, 1, 1);
+
+  const material = new THREE.MeshBasicMaterial({
+    map: new THREE.TextureLoader().load("public/img/test.jpg"),
+  });
+
+  const mesh = new THREE.Mesh(geometry, material);
+  scene.add(mesh);
+
+  const helperGeometry = new THREE.BoxBufferGeometry(100, 100, 100, 4, 4, 4);
+  const helperMaterial = new THREE.MeshBasicMaterial({
+    color: 0xff00ff,
+    wireframe: true,
+  });
+  const helper = new THREE.Mesh(helperGeometry, helperMaterial);
+  scene.add(helper);
+
+  //
+
+  renderer = new THREE.WebGLRenderer({ antialias: true });
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
+
+  //
+
+  window.addEventListener("resize", onWindowResize, false);
+}
+
+function animate() {
+  window.requestAnimationFrame(animate);
+
+  controls.update();
+  renderer.render(scene, camera);
+}
+
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
